abacbd039a98e5332a199360d2dd5c7ad06629cf
infinite1by0 <91357272+infinite1by0@users.noreply.github.com>
2022-05-19 19:28:30 +0530

    autoread disable

--- Gojosensei.js
+++ Gojosensei.js
@@ -213,10 +213,10 @@ const reply = (teks) => {
         }
 
         //Push Message To Console && Auto Read\\
-        if (m.message) {
-            GojoMdNx.sendReadReceipt(m.chat, m.sender, [m.key.id])
-            console.log(chalk.black(chalk.bgWhite('[ MESSAGE ]')), chalk.black(chalk.bgGreen(new Date)), chalk.black(chalk.bgBlue(budy || m.mtype)) + '\n' + chalk.magenta('=> From'), chalk.green(pushname), chalk.yellow(m.sender) + '\n' + chalk.blueBright('=> In'), chalk.green(m.isGroup ? pushname : 'Private Chat', m.chat))
-        }
+//         if (m.message) {
+//             GojoMdNx.sendReadReceipt(m.chat, m.sender, [m.key.id])
+//             console.log(chalk.black(chalk.bgWhite('[ MESSAGE ]')), chalk.black(chalk.bgGreen(new Date)), chalk.black(chalk.bgBlue(budy || m.mtype)) + '\n' + chalk.magenta('=> From'), chalk.green(pushname), chalk.yellow(m.sender) + '\n' + chalk.blueBright('=> In'), chalk.green(m.isGroup ? pushname : 'Private Chat', m.chat))
+//         }
 	
 	//reset limit every 12 hours\\
         let cron = require('node-cron')
